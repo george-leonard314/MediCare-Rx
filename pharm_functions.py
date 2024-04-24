d483af04427c81a2e171c85ad58436e7ae6799e1
George <g.batca@student.fontys.nl>
2024-04-24 02:11:06 +0200

    First Try of running the CLI

--- pharm_functions.py
+++ pharm_functions.py
@@ -6,13 +6,25 @@ from config import CONFIG
 # TODO Functions
 
 def add_stock(med_name, med_quantity, med_price, med_description):
-    pass
+    new_stock = {
+        "med_name": med_name,
+        "med_quantity": med_quantity,
+        "med_price": med_price,
+        "med_description": med_description
+    }
+    response = requests.post(f"{CONFIG['api']['url']}/stock, json=new_med")
 
 def remove_stock(med_id):
-    pass
+    requests.delete(f"{CONFIG['api']['url']}/stock/{med_id}")
 
 def list_stock():
-    pass
+    results = requests.get(f"{CONFIG['api']['url']}/stock")
+    stock = json.loads(results.text)
 
-def update_stock(med_id, med_quantity):
-    pass
+    return stock
+
+def update_stock(med_id, med, med_quantity):
+    response = requests.put( f"{CONFIG['api']['url']}/stock/{med['med_id']}", json=med)
+    med = json.loads(response.text)
+
+    return int(med["med_id"])
